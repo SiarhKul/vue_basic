33e1d686ae5782ac8e1a432c13ef243945fda98b
Siarhei Kulazhanka <skulazhanka@exadel.com>
2022-06-28 15:37:35 +0300

    feat: Crated first component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,18 @@
 <template>
-<h1>HI111</h1>
+  <the-header></the-header>
 </template>
 
 <script>
 
+import TheHeader from '@/TheHeader'
+
 export default {
   name: 'App',
   components: {
+    TheHeader
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
 </style>
