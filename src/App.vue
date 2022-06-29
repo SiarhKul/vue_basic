2d4b4dda889b6b20e7e15e930a43f01674866b63
Siarhei Kulazhanka <skulazhanka@exadel.com>
2022-06-29 11:27:58 +0300

    init: Added slots

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,34 @@
     <div class='card'>
       <h2>Slots</h2>
     </div>
+    <app-list>
+      <template #default='slotProps'>
+        <span style='color: red'>{{ slotProps.iter }}</span>
+      </template>
+    </app-list>
+    <app-block>
+      <p>Text for slot from App</p>
+
+      <template #header>
+        <h3>This is heading from App</h3>
+      </template>
+
+      <template #footer>
+        <hr>
+        <small>Thi is footer from App</small>
+      </template>
+
+    </app-block>
   </div>
 </template>
 
 <script>
+import AppBlock from '@/AppBlock';
+import AppList from '@/AppList';
 
 export default {
   name: 'App',
-  components: {},
-
+  components: { AppBlock, AppList },
 };
 </script>
 
---
+++ src/AppBlock.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class='card'>
+    <h1>Title from AppBlock</h1>
+    <slot name='header'>Header is not provided</slot>
+    <slot>Default text is not provided</slot>
+    <slot name='footer'>Footer is not provided</slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+
+</style>
