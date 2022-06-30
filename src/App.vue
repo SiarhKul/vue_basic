bf4bca139e7227376e9a69df4cc9018f7564e08c
Siarhei Kulazhanka <skulazhanka@exadel.com>
2022-06-30 10:40:50 +0300

    feat: Added form

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 <template>
   <div class='container'>
-    <form class='card'>
+    <form class='card' @submit.prevent='submitForm'>
       <h1>Анкета на Vue разработчика!</h1>
-      <div class='form-control'>
-        <label for='name'>Как тебя зовут?</label>
-        <input type='text' id='name' placeholder='Введи имя'>
-      </div>
+      <!--      <div class='form-control' :class='{invalid:error.name}'>-->
+      <!--        <label for='name'>Как тебя зовут?</label>-->
+      <!--        <input type='text' id='name' placeholder='Введи имя' v-model.trim='name'>-->
+      <!--        <small v-if='error.name'>{{ error.name }}</small>-->
+      <!--      </div>-->
+
+      <app-input placeholder='Enter your name'
+                 :error='error.name'
+                 label='name'
+                 v-model='name'>
+      </app-input>
 
       <div class='form-control'>
         <label for='age'>Выбери возраст</label>
-        <input type='number' id='age' value='20'>
+        <input type='number' id='age' max='70' v-model.number='age'>
       </div>
 
       <div class='form-control'>
         <label for='city'>Твой город</label>
-        <select id='city'>
+        <select id='city' v-model='city'>
           <option value='spb'>Санкт-Петербург</option>
           <option value='msk'>Москва</option>
           <option value='kzn'>Казань</option>
@@ -25,24 +32,25 @@
       <div class='form-checkbox'>
         <span class='label'>Готов к переезду в Токио?</span>
         <div class='checkbox'>
-          <label><input type='radio' name='trip' /> Да</label>
+          <label><input type='radio' name='trip' value='yes' v-model='relocate' /> Да</label>
         </div>
 
         <div class='checkbox'>
-          <label><input type='radio' name='trip' /> Нет</label>
+          <label><input type='radio' name='trip' value='no' v-model='relocate' /> Нет</label>
         </div>
       </div>
 
       <div class='form-checkbox'>
         <span class='label'>Что знаешь во Vue?</span>
         <div class='checkbox'>
-          <label><input type='checkbox' /> Vuex</label>
+          <label><input type='checkbox' name='skills' v-model='skills' value='vuex' /> Vuex</label>
         </div>
         <div class='checkbox'>
-          <label><input type='checkbox' /> Vue CLI</label>
+          <label><input type='checkbox' name='skills' v-model='skills' value='cli' /> Vue CLI</label>
         </div>
         <div class='checkbox'>
-          <label><input type='checkbox' /> Vue Router</label>
+          <label><input type='checkbox' name='skills' v-model='skills' value='router' /> Vue Router</label>
+
         </div>
       </div>
 
@@ -52,9 +60,56 @@
 </template>
 
 <script>
-export default {};
+import AppInput from '@/AppInput';
+
+export default {
+  components: { AppInput },
+  data() {
+    return {
+      name: '',
+      age: 23,
+      city: 'spb',
+      relocate: 'yes',
+      skills: ['vuex'],
+      error: { name: null },
+    };
+
+  },
+  methods: {
+    formValid() {
+      let isValid = true;
+      if (this.name.length === 0) {
+        this.error.name = 'Enter your name';
+        isValid = false;
+      } else {
+        this.error.name = null;
+      }
+
+      return isValid;
+    },
+
+    submitForm() {
+      if (this.formValid()) {
+        console.group('form Data');
+        console.log('name', this.name);
+        console.log('age', this.age);
+        console.log('city', this.city);
+        console.log('relocate', this.relocate);
+        console.log('skills', this.skills);
+        console.groupEnd();
+      }
+    },
+  },
+};
 </script>
 
 <style>
+.form-control small {
+  color: red;
+}
+
+.form-control.invalid input {
+  border-color: #c25205;
+}
 
 </style>
