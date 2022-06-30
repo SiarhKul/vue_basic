d0944952f159be9049fd31afddbc2d9f4f7d47fe
Siarhei Kulazhanka <skulazhanka@exadel.com>
2022-06-30 14:56:41 +0300

    feat: Create basic Http functionality

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,117 @@
 <template>
   <div class='container'>
-    <div class='card'>
+    <app-alert :alert='alert' @close='alert=null'></app-alert>
+    <form class='card' @submit.prevent='createPerson'>
       <h2>Работа с базой данных</h2>
-    </div>
+
+      <div class='form-control'>
+        <label for='name'>Enter name</label>
+        <input type='text' id='name' v-model.trim='name'>
+      </div>
+      <button class='btn primary' :disabled='name.length===0'>Submit</button>
+    </form>
+
+    <app-loader v-if='loading'></app-loader>
+
+    <app-people-list
+      v-else
+      :people='people'
+      @load='loadPeople'
+      @remove='removePerson'
+    ></app-people-list>
   </div>
 </template>
-
+//-------------------------------------------------JS
 <script>
-export default {};
-</script>
+import AppPeopleList from '@/AppPeopleList';
+import AppAlert from '@/AppAlert';
+import AppLoader from '@/AppLoader';
+import axios from 'axios';
 
+export default {
+
+  data() {
+    return {
+      name: '',
+      people: [],
+      alert: null,
+      loading: false,
+    };
+  },
+
+  mounted() {
+    this.loadPeople();
+  },
+  methods: {
+    async createPerson() {
+      const response = await fetch('https://vue-with-http-2ae88-default-rtdb.europe-west1.firebasedatabase.app/people.json', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          firstName: this.name,
+        }),
+      });
+
+      const firebaseData = await response.json();
+
+      this.people.push({
+        firstName: this.name,
+        id: firebaseData.name,
+      });
+
+      this.name = '';
+
+    },
+
+    async loadPeople() {
+      try {
+        this.loading = true;
+        const { data } = await axios.get('https://vue-with-http-2ae88-default-rtdb.europe-west1.firebasedatabase.app/people.json');
+        if (!data) {
+          throw new Error('List of people it empty');
+        }
+
+        this.people = Object.keys(data).map(key => {
+          return {
+            id: key,
+            ...data[key],
+          };
+        });
+
+        this.loading = false;
+      } catch (e) {
+        this.alert = {
+          type: 'danger',
+          title: 'Error',
+          text: e.message,
+        };
+        this.loading = false;
+      }
+    },
+
+    async removePerson(id) {
+      console.log(id);
+
+      try {
+        const name = this.people.find(person => person.id === id).firstName;
+        await axios.delete(`https://vue-with-http-2ae88-default-rtdb.europe-west1.firebasedatabase.app/people/${id}.json`);
+        this.people = this.people.filter(person => person.id !== id);
+        this.alert = {
+          type: 'primary',
+          title: 'Success',
+          text: `User ${name} was  removed`,
+        };
+      } catch (e) {
+        console.log(e.message);
+      }
+    },
+  },
+  components: { AppLoader, AppPeopleList, AppAlert },
+};
+</script>
+//-------------------------------------------------CSS
 <style>
 
 </style>
---
+++ src/AppPeopleList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-if='people.length!==0'>
+    <div class='card inline' v-for='person in people' :key='person.id'>
+      <h3>{{ person.firstName }}</h3>
+      <button
+        class='btn danger'
+        @click='$emit("remove",person.id)'
+      >Remove
+      </button>
+    </div>
+  </div>
+  <div class='card center' v-else>
+    <h4>Load people</h4>
+    <button class='btn' @click='$emit("load")'> Load list of people</button>
+  </div>
+
+
+</template>
+
+<script>
+export default {
+  name: 'AppPeopleList',
+  props: ['people'],
+  emits: ['load', 'remove'],
+};
+
+</script>
+
+<style scoped>
+.inline {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
