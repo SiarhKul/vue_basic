c36a5160f0a3cc2df86fb20fddc111e1506194f2
Siarhei Kulazhanka <skulazhanka@exadel.com>
2022-07-01 13:18:37 +0300

    init: Initialisation directions

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,14 @@
 <template>
   <div class='container'>
-    <app-alert v-if='alert' text='It is important!' title='Attraction!' type='danger' closable
-               @close='alert=false'></app-alert>
-    <div class='card'>
-      <button class='btn primary' @click='toggleAlert'>{{ alert ? 'Hide' : 'Show' }}</button>
-    </div>
-    <app-block></app-block>
+    <div class='card'>Directives</div>
   </div>
 </template>
 
 <script>
-import AppAlert from '@/components/AppAlert';
-import AppBlock from '@/components/AppBlock';
-import AlertMixin from '@/AlertMixin';
 
 export default {
   name: 'App',
-  components: { AppAlert, AppBlock },
-  mixins: [AlertMixin],
+  components: {},
 
 };
 </script>
