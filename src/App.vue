ab4820764816a497c12b7dcfffcec3be059091b5
Siarhei Kulazhanka <skulazhanka@exadel.com>
2022-07-01 14:54:32 +0300

    init: Initialisation plagin

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,16 @@
 <template>
   <div class='container'>
-    <div class='card' v-if='show'>
-
-      <h2 v-color:[type].blink.hover='myColor'>Directions</h2>
-
-      <div class='form-control'>
-        <label for='inp'>Default Input</label>
-        <input v-focus type='text' id='inp'>
-      </div>
-
-      <button class='btn' @click='myColor="darkblue"'>Change</button>
-      <button class='btn' @click='type=type==="color"?"backgroundColor":"color"'>Change type</button>
-    </div>
+    <div class='card'>Plugins</div>
   </div>
 </template>
 
 <script>
-import focusDirective from '@/focusDirective';
-import colorDirective from '@/colorDirective';
 
 export default {
   name: 'App',
   components: {},
   data() {
-    return {
-      myColor: 'darkred',
-      type: 'color',
-      show: true,
-    };
-  },
-  mounted() {
-    // setTimeout(() => {
-    //   this.show = false;
-    // }, 2000);
-  },
-  directives: {
-    focus: focusDirective,
-    color: colorDirective,
+    return {};
   },
 
 };
---
+++ src/AppDirective.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class='container'>
+    <div class='card' v-if='show'>
+
+      <h2 v-color:[type].blink.hover='myColor'>Directions</h2>
+
+      <div class='form-control'>
+        <label for='inp'>Default Input</label>
+        <input v-focus type='text' id='inp'>
+      </div>
+
+      <button class='btn' @click='myColor="darkblue"'>Change</button>
+      <button class='btn' @click='type=type==="color"?"backgroundColor":"color"'>Change type</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import focusDirective from '@/focusDirective';
+import colorDirective from '@/colorDirective';
+
+export default {
+  name: 'App',
+  components: {},
+  data() {
+    return {
+      myColor: 'darkred',
+      type: 'color',
+      show: true,
+    };
+  },
+  mounted() {
+    // setTimeout(() => {
+    //   this.show = false;
+    // }, 2000);
+  },
+  directives: {
+    focus: focusDirective,
+    color: colorDirective,
+  },
+
+};
+</script>
+
