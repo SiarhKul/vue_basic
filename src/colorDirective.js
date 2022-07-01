edc9e46a9f71463625b8fde96cf8806b0a314ec5
Siarhei Kulazhanka <skulazhanka@exadel.com>
2022-07-01 14:51:17 +0300

    feat: Added custom directions

---
+++ src/colorDirective.js
@@ -0,0 +1,50 @@
+let interval;
+
+let defaultColor;
+
+const mouseover = e => {
+  e.target.style.color = 'yellow';
+};
+
+const mouseout = e => {
+  e.target.style.color = defaultColor;
+
+};
+
+export default {
+  mounted(el, binding) {
+    console.log('----->>', el);
+    console.log('----->>', binding);
+    defaultColor = binding.value;
+
+    el.style[binding.arg] = binding.value;
+
+    if (binding.modifiers.blink) {
+      let flag = true;
+      interval = setInterval(() => {
+        el.style.color = flag ? 'white' : binding.value;
+        flag = !flag;
+      }, 1000);
+    }
+
+    if (binding.modifiers.hover) {
+      el.addEventListener('mouseover', mouseover);
+      el.addEventListener('mouseout', mouseout);
+    }
+
+
+  },
+
+  updated(el, binding) {
+    el.style[binding.arg] = binding.value;
+  },
+
+  unmounted(el) {
+    if (interval) {
+      clearInterval(interval);
+
+      el.removeEventListener('mouseover', mouseover);
+      el.removeEventListener('mouseout', mouseout);
+    }
+  },
+};
