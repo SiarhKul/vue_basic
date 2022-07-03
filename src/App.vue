0f76e6df3433fb0b4315a1efce0ba38d432be87a
skulazhanka <SiarhKul@gmail.com>
2022-07-03 17:40:21 +0300

    feat: Added validation

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,52 @@
 <template>
-  <div class="container column">
-    <form class="card card-w30">
-      <div class="form-control">
-        <label for="type">Тип блока</label>
-        <select id="type">
-          <option value="title">Заголовок</option>
-          <option value="subtitle">Подзаголовок</option>
-          <option value="avatar">Аватар</option>
-          <option value="text">Текст</option>
+  <div class='container column'>
+    <form class='card card-w30' @submit.prevent='onSubmit'>
+      <div class='form-control'>
+        <label for='type'>Тип блока</label>
+        <select id='type' v-model='cvItem'>
+          <option value='title'>Заголовок</option>
+          <option value='subtitle'>Подзаголовок</option>
+          <option value='avatar'>Аватар</option>
+          <option value='text'>Текст</option>
         </select>
       </div>
 
-      <div class="form-control">
-        <label for="value">Значение</label>
-        <textarea id="value" rows="3"></textarea>
+      <div class='form-control'>
+        <label for='value'>Значение</label>
+        <textarea id='value' rows='3' v-model='cvItemDescription'></textarea>
+        <small v-if='error'> Колличестов символов должно быть больше 3</small>
       </div>
 
-      <button class="btn primary">Добавить</button>
+      <button class='btn primary'>Добавить</button>
     </form>
 
-    <div class="card card-w70">
+    <div class='card card-w70'>
+      <app-nick-name v-if='cvItem==="title"'></app-nick-name>
       <h1>Резюме Nickname</h1>
-      <div class="avatar">
-        <img src="https://cdn.dribbble.com/users/5592443/screenshots/14279501/drbl_pop_r_m_rick_4x.png">
+      <div class='avatar'>
+        <img src='https://cdn.dribbble.com/users/5592443/screenshots/14279501/drbl_pop_r_m_rick_4x.png'>
       </div>
       <h2>Опыт работы</h2>
       <p>
-        главный герой американского мультсериала «Рик и Морти», гениальный учёный, изобретатель, атеист (хотя в некоторых сериях он даже молится Богу, однако, каждый раз после чудесного спасения ссылается на удачу и вновь отвергает его существование), алкоголик, социопат, дедушка Морти. На момент начала третьего сезона ему 70 лет[1]. Рик боится пиратов, а его главной слабостью является некий - "Санчезиум". Исходя из того, что существует неограниченное количество вселенных, существует неограниченное количество Риков, герой сериала предположительно принадлежит к измерению С-137. В серии комикcов Рик относится к измерению C-132, а в игре «Pocket Mortys» — к измерению C-123[2]. Прототипом Рика Санчеза является Эмметт Браун, герой кинотрилогии «Назад в будущее»[3].
+        главный герой американского мультсериала «Рик и Морти», гениальный учёный, изобретатель, атеист (хотя в
+        некоторых сериях он даже молится Богу, однако, каждый раз после чудесного спасения ссылается на удачу и вновь
+        отвергает его существование), алкоголик, социопат, дедушка Морти. На момент начала третьего сезона ему 70
+        лет[1]. Рик боится пиратов, а его главной слабостью является некий - "Санчезиум". Исходя из того, что существует
+        неограниченное количество вселенных, существует неограниченное количество Риков, герой сериала предположительно
+        принадлежит к измерению С-137. В серии комикcов Рик относится к измерению C-132, а в игре «Pocket Mortys» — к
+        измерению C-123[2]. Прототипом Рика Санчеза является Эмметт Браун, герой кинотрилогии «Назад в будущее»[3].
       </p>
-      <h3>Добавьте первый блок, чтобы увидеть результат</h3>
+      <h3 v-if='isObjectEmpty'>Добавьте первый блок, чтобы увидеть результат</h3>
     </div>
   </div>
-  <div class="container">
+  <div class='container'>
     <p>
-      <button class="btn primary">Загрузить комментарии</button>
+      <button class='btn primary'>Загрузить комментарии</button>
     </p>
-    <div class="card">
+    <div class='card'>
       <h2>Комментарии</h2>
-      <ul class="list">
-        <li class="list-item">
+      <ul class='list'>
+        <li class='list-item'>
           <div>
             <p><strong>test@microsoft.com</strong></p>
             <small>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi, reiciendis.</small>
@@ -46,14 +54,52 @@
         </li>
       </ul>
     </div>
-    <div class="loader"></div>
+    <div class='loader'></div>
   </div>
 </template>
 
 <script>
-export default {
+import AppNickName from '@/components/AppNickName';
 
-}
+export default {
+  data() {
+    return {
+      cvItem: 'title',
+      cvItemDescription: '',
+      error: false,
+      cvBlocks: {},
+    };
+  },
+  methods: {
+    isFormValid() {
+      let isValidValue = true;
+      if (this.cvItemDescription.length <= 3) {
+        isValidValue = false;
+      }
+      return isValidValue;
+    },
+    onSubmit() {
+      if (this.isFormValid()) {
+        this.cvBlocks[this.cvItem] = {
+          cvBlock: this.cvItem,
+          cvBlockDescription: this.cvItemDescription,
+        };
+        this.error = false;
+        console.log(this.cvBlocks);
+      } else {
+        this.error = true;
+      }
+    },
+  },
+  computed: {
+    isObjectEmpty() {
+      return Object.keys(this.cvBlocks).length === 0;
+    },
+  },
+  components: {
+    AppNickName,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/AppNickName.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>SiarhKul</h1>
+</template>
+
+<script>
+export default {
+  name: 'AppNickName',
+};
+</script>
+
+<style scoped>
+
+</style>
